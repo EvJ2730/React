c1fb8bb6e996ac0b9277436283e426a0520de347
EvJ <73695353+EvJ2730@users.noreply.github.com>
2023-01-25 18:44:01 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { Header } from "./components/Header";
+//import React from "react"; = must have if using classes
+
+/*class App extends React.Component {
+  render() {
+    return <h1>Hello from a class</h1>
+  }
+}*/
 
 function App() {
+  //const name = 'Eva'
+  //const x = true
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+{/* <h1>Hello From {name} {5+3} {x ? "Yes" : "No"}</h1>
+    <h1>Hello From {name} </h1> */}
+     <Header />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,32 @@
+import PropTypes from 'prop-types'
+import { Button } from './Button'
+
+export const Header = ({title}) => {
+    const onClick = () => {
+        console.log("Click")
+    }
+  return (
+    <header className='header'>
+        
+        {/* <h1 style={{color: "DodgerBlue", backgroundColor:"yellowgreen"}}>{title}</h1> */}
+        {/* <h2 style={headingStyle}>Hello</h2> */}
+        
+        <h1>{title}</h1>
+        <Button color = "green" text = "Add" onClick={onClick}/>
+
+    </header>
+  )
+}
+
+Header.defaultProps = {
+    title: "Task Tracker",
+}
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
+}
+//CSS in JS
+// const headingStyle = {
+//     color: "darkMagenta",
+//     backgroundColor: "plum"
+// }
